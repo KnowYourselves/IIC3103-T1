084084ef8c12aebfa0891b0761b288a727ecd41b
Raul Alvarez <raul.alvarez@magnet.cl>
2021-04-05 19:12:05 -0400

    🎉 Finished home page dark mode

--- app/components/modules/navbar/components/searchbar.js
+++ app/components/modules/navbar/components/searchbar.js
@@ -16,7 +16,7 @@ const Searchbar = () => {
   return (
     <div className="relative w-full group sm:w-auto">
       <input
-        className="w-full h-10 pl-5 pr-12 text-sm bg-white border-green-100 rounded-lg sm:w-72 border-3 focus:ring-2 focus:outline-none focus:ring-green-600"
+        className="w-full h-10 pl-5 pr-12 text-sm text-black bg-white border-green-100 rounded-lg sm:w-72 border-3 focus:ring-2 focus:outline-none dark:focus:ring-2 focus:ring-green-600 dark:focus:ring-gray-500"
         type="text"
         placeholder="Search"
         onKeyDown={onKeyDown}
@@ -26,7 +26,7 @@ const Searchbar = () => {
         className="absolute top-0 right-0 mt-2 mr-4"
         type="button"
       >
-        <svg className="w-6 h-6 text-gray-400 transition duration-500 group-hover:text-green-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg className="w-6 h-6 text-gray-400 transition dark:group-hover:text-gray-900 hover:duration-200 group-hover:text-green-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
       </button>
